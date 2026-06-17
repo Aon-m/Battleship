ae2df3c04713514af4a00077b4f79bcb06d33e15
Aon <aeolaojo@gmail.com>
2026-06-17 10:52:06 +0100

    feat: create templates for the screens

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles/styles.css";
+
+document.querySelectorAll(".button--primary").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.remove("floating-hover");
+    btn.classList.add("yeet");
+  });
+});
